0a83c6530c123a0195922dd634d50ef32f9d35b7
msk226 <kminseok5167@gmail.com>
2024-07-11 15:39:50 +0900

    [add] 초기 파일 업로드

---
+++ src/test/java/com/example/spot/SpotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
