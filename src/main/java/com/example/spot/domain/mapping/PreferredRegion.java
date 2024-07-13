023de8a833f473c8a33eca3c337c0b5be651b713
dvlp-sy <sy8389b@gmail.com>
2024-07-13 16:04:18 +0900

    [add] Create PreferredRegion Domain Class

---
+++ src/main/java/com/example/spot/domain/mapping/PreferredRegion.java
@@ -0,0 +1,25 @@
+package com.example.spot.domain.mapping;
+
+import com.example.spot.domain.Member;
+import com.example.spot.domain.Region;
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Getter
+@Entity
+public class PreferredRegion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, unique = true)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "region_id", nullable = false)
+    private Region region;
+
+}
