88eae41195429a1dad6a81eec16ba03ffbf6d5a4
msk226 <kminseok5167@gmail.com>
2024-07-13 15:18:43 +0900

    [feat] StudyLikedPost 엔티티 생성

---
+++ src/main/java/com/example/domain/StudyLikedPost.java
@@ -0,0 +1,36 @@
+package com.example.domain;
+
+import com.example.domain.common.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+@Entity
+@Getter
+@Builder
+@DynamicUpdate
+@DynamicInsert
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class StudyLikedPost extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private StudyPost studyPost;
+
+    //private Member member;
+
+}
