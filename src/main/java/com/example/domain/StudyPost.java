e6342f13065163a992fa92254b135c34d737ffed
msk226 <kminseok5167@gmail.com>
2024-07-13 15:12:00 +0900

    [feat] StudyPost 및 Theme 엔티티 생성

---
+++ src/main/java/com/example/domain/StudyPost.java
@@ -0,0 +1,60 @@
+package com.example.domain;
+
+import com.example.domain.common.BaseEntity;
+import com.example.domain.enums.Theme;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.criteria.CriteriaBuilder.In;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+@Entity
+@Getter
+@Builder
+@DynamicUpdate
+@DynamicInsert
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class StudyPost extends BaseEntity {
+
+    @Id @GeneratedValue
+    private long id;
+
+    @Column(nullable = false)
+    private boolean isAnnouncement;
+
+    @Column(nullable = false)
+    private LocalDateTime announcedAt;
+
+    @Column(nullable = false)
+    private Theme theme;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private Integer likeCount;
+
+    @Column(nullable = false)
+    private Integer commentCount;
+
+
+
+
+
+
+
+
+
+}
---
+++ src/main/java/com/example/domain/enums/Theme.java
@@ -0,0 +1,7 @@
+package com.example.domain.enums;
+
+public enum Theme {
+
+    ON, OFF
+
+}
