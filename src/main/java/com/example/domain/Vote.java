92d504bd20c630f75355f7f969df7b1162c64b72
msk226 <kminseok5167@gmail.com>
2024-07-13 15:27:11 +0900

    [feat] Vote 엔티티 생성

--- src/main/java/com/example/domain/Vote.java
+++ src/main/java/com/example/domain/Vote.java
@@ -1,9 +1,13 @@
 package com.example.domain;
 
 import com.example.domain.common.BaseEntity;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -24,6 +28,18 @@ public class Vote extends BaseEntity {
     @Id @GeneratedValue
     private long id;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Study study;
 
+    //private Member member;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private boolean isMultipleChoice;
+
+    @Column(nullable = false)
+    private LocalDateTime finishedAt;
 
 }
