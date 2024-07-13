127179567e5df8471b7f7f0306c1c58929af2add
msk226 <kminseok5167@gmail.com>
2024-07-13 15:15:58 +0900

    [feat] StudyPostComment 엔티티 생성

--- src/main/java/com/example/domain/StudyPostComment.java
+++ src/main/java/com/example/domain/StudyPostComment.java
@@ -1,12 +1,12 @@
 package com.example.domain;
 
 import com.example.domain.common.BaseEntity;
-import com.example.domain.enums.Theme;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import java.time.LocalDateTime;
+import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -22,22 +22,16 @@ import org.hibernate.annotations.DynamicUpdate;
 @DynamicInsert
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
-public class StudyPost extends BaseEntity {
+public class StudyPostComment extends BaseEntity {
 
     @Id @GeneratedValue
     private long id;
 
-    @Column(nullable = false)
-    private boolean isAnnouncement;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private StudyPost studyPost;
 
-    @Column(nullable = false)
-    private LocalDateTime announcedAt;
-
-    @Column(nullable = false)
-    private Theme theme;
-
-    @Column(nullable = false)
-    private String title;
+    // @Column(nullable = false)
+    // private Member member;
 
     @Column(nullable = false)
     private String content;
@@ -45,7 +39,8 @@ public class StudyPost extends BaseEntity {
     @Column(nullable = false)
     private Integer likeCount;
 
-    @Column(nullable = false)
-    private Integer commentCount;
+
+
+
 
 }
