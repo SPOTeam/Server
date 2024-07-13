80480a1bc29c13ac66d6762108b358655e69458b
dvlp-sy <sy8389b@gmail.com>
2024-07-13 16:04:41 +0900

    [add] Create StudyTheme Domain Class

---
+++ src/main/java/com/example/spot/domain/mapping/StudyTheme.java
@@ -0,0 +1,26 @@
+package com.example.spot.domain.mapping;
+
+import com.example.spot.domain.Study;
+import com.example.spot.domain.Theme;
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Getter
+@Entity
+public class StudyTheme {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, unique = true)
+    private Long id;
+
+    //== 테마 ==//
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "theme_id", nullable = false)
+    private Theme theme;
+
+    //== 스터디 ==//
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "study_id", nullable = false)
+    private Study study;
+}
