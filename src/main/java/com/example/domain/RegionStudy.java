8121fefc4356913b1cf4dddb2b41f105635ef7b2
kyoungmin <kkyoung0611@naver.com>
2024-07-15 20:51:52 +0900

    feat: add RegionStudy Entity

---
+++ src/main/java/com/example/domain/RegionStudy.java
@@ -0,0 +1,26 @@
+package com.example.domain;
+
+import com.example.spot.domain.common.BaseEntity;
+import com.example.spot.domain.study.Study;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class RegionStudy extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "region_id")
+    private Region region;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "study_id")
+    private Study study;
+}
