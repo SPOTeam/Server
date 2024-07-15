298da1dcd6a2326b0fa39b6dfb45b02370c62795
kyoungmin <kkyoung0611@naver.com>
2024-07-15 20:51:34 +0900

    feat: add PostReport Entity

---
+++ src/main/java/com/example/domain/PostReport.java
@@ -0,0 +1,31 @@
+package com.example.domain;
+
+import com.example.spot.domain.Member;
+import com.example.spot.domain.Post;
+import com.example.spot.domain.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PostReport extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+
+    @Enumerated(EnumType.STRING)
+    private PostStatus postStatus;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+}
