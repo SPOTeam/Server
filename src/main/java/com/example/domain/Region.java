83288e459271d4d8e0ed1516193923cf04c6a43b
kyoungmin <kkyoung0611@naver.com>
2024-07-15 20:51:43 +0900

    feat: add Region Entity

---
+++ src/main/java/com/example/domain/Region.java
@@ -0,0 +1,34 @@
+package com.example.domain;
+
+import com.example.spot.domain.mapping.PreferredRegion;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Region {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String code;
+
+    private String province;
+
+    private String district;
+
+    private String neighborhood;
+
+    @OneToMany(mappedBy = "region")
+    private List<RegionStudy> regionStudyList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "region")
+    private List<PreferredRegion> prefferedRegionList = new ArrayList<>();
+
+}
