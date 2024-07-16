bf6a5e882701abc281db8a685987b424a973b9af
dvlp-sy <sy8389b@gmail.com>
2024-07-16 17:27:21 +0900

    [add] Create Controller Files
    - with example codes

---
+++ src/main/java/com/example/spot/controller/NotificationController.java
@@ -0,0 +1,11 @@
+package com.example.spot.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "Notification", description = "Notification API")
+@RestController
+@RequestMapping("/api")
+public class NotificationController {
+}
