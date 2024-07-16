0083376c107a0869e2fc82ae8c41166ee07ba50d
ISU <ourwavelets@gmail.com>
2024-07-16 18:58:18 +0900

    [add] Create GeneralException Class

---
+++ src/main/java/com/example/spot/api/exception/GeneralException.java
@@ -0,0 +1,21 @@
+package com.example.spot.api.exception;
+
+import com.example.spot.api.code.BaseErrorCode;
+import com.example.spot.api.code.ErrorReasonDTO;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class GeneralException extends RuntimeException {
+
+    private BaseErrorCode code;
+
+    public ErrorReasonDTO getErrorReason() {
+        return this.code.getReason();
+    }
+
+    public ErrorReasonDTO getErrorReasonHttpStatus() {
+        return this.code.getReasonHttpStatus();
+    }
+}
