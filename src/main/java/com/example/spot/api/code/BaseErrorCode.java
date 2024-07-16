a2e1e9bd72031408dd384b35e6aacefe0292d7f8
ISU <ourwavelets@gmail.com>
2024-07-16 18:56:43 +0900

    [rename] 클래스명 수정

--- src/main/java/com/example/spot/api/code/BaseErrorCode.java
+++ src/main/java/com/example/spot/api/code/BaseErrorCode.java
@@ -2,7 +2,7 @@ package com.example.spot.api.code;
 
 public interface BaseErrorCode {
 
-    public ErrorReason getReason();
+    public ErrorReasonDTO getReason();
 
-    public ErrorReason getReasonHttpStatus();
+    public ErrorReasonDTO getReasonHttpStatus();
 }
--- src/main/java/com/example/spot/api/code/Reason.java
+++
@@ -1,4 +0,0 @@
-package com.example.spot.api.code;
-
-public class Reason {
-}
