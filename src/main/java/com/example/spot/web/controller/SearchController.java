05691425b61e5425ea40772c68877f2d360051fc
msk226 <kminseok5167@gmail.com>
2024-07-17 14:50:00 +0900

    [docs] 내 관심 지역 별 스터디 조회 API 명세서 초안 작성

--- src/main/java/com/example/spot/web/controller/SearchController.java
+++ src/main/java/com/example/spot/web/controller/SearchController.java
@@ -33,9 +33,12 @@ public class SearchController {
     @Parameter(name = "userId", description = "접속한 유저의 ID를 입력 받습니다.", required = true)
     public void recommendStudiesForMain(@PathVariable long userId) {}
 
-    @GetMapping("/search/users/{userId}/interest-studies/all")
+
+    /* ----------------------------- 내 관심 분야 별 스터디 조회  ------------------------------------- */
+
+    @GetMapping("/search/users/{userId}/interest-themes/studies/all")
     @Operation(
-        summary = "[내 관심사 스터디 조회 - 개발중] '전체' 관심 분야 스터디 조회",
+        summary = "[내 관심사 스터디 조회 - 개발중] 내 '전체' 관심 분야 스터디 조회",
         description = """
             ## [내 관심사 스터디 조회] 접속한 회원의 모든 관심 분야의 스터디를 전체 조회 합니다.
             
@@ -47,9 +50,9 @@ public class SearchController {
         // 메소드 구현
     }
 
-    @GetMapping("/search/users/{userId}/interest-studies/specific")
+    @GetMapping("/search/users/{userId}/interest-themes/studies/specific")
     @Operation(
-        summary = "[내 관심사 스터디 조회 - 개발중] '특정' 관심 분야 스터디 조회",
+        summary = "[내 관심사 스터디 조회 - 개발중] 내 '특정' 관심 분야 스터디 조회",
         description = """
             ## [내 관심사 스터디 조회] 접속한 회원의 특정 관심 분야의 스터디를 조회 합니다.
             
@@ -65,7 +68,7 @@ public class SearchController {
         // 메소드 구현
     }
 
-    @GetMapping("/search/users/{userId}/interest-studies/conditions/all")
+    @GetMapping("/search/users/{userId}/interest-themes/studies/conditions/all")
     @Operation(
         summary = "[내 관심사 스터디 조회 - 개발중] 특정 조건 + 내 '전체' 관심사 스터디 조회",
         description = """
@@ -91,7 +94,7 @@ public class SearchController {
         // 메소드 구현
     }
 
-    @GetMapping("/search/users/{userId}/interest-studies/conditions/specific")
+    @GetMapping("/search/users/{userId}/interest-themes/studies/conditions/specific")
     @Operation(
         summary = "[내 관심사 스터디 조회 - 개발중] 특정 조건 + 내 '특정' 관심사 스터디 조회",
         description = """
@@ -120,7 +123,90 @@ public class SearchController {
     }
 
 
+    /* ----------------------------- 내 관심 지역 별 스터디 조회  ------------------------------------- */
+
+    @GetMapping("/search/users/{userId}/preferred-region/studies/all")
+    @Operation(
+        summary = "[내 관심 지역 스터디 조회 - 개발중] 내 '전체' 관심 지역 스터디 조회",
+        description = """
+            ## [내 관심 지역 스터디 조회] 접속한 회원의 모든 관심 지역의 스터디를 전체 조회 합니다.
+            
+            조회된 전체 스터디 목록이 반환 됩니다.""",
+        security = @SecurityRequirement(name = "accessToken")
+    )
+    @Parameter(name = "userId", description = "접속한 유저의 ID를 입력 받습니다.", required = true)
+    public void interestRegionStudies(@PathVariable long userId) {
+        // 메소드 구현
+    }
+
+    @GetMapping("/search/users/{userId}/preferred-region/studies/specific")
+    @Operation(
+        summary = "[내 관심 지역 스터디 조회 - 개발중] 내 '특정' 관심 지역 스터디 조회",
+        description = """
+            ## [내 관심 지역 스터디 조회] 접속한 회원의 특정 관심 지역의 스터디를 조회 합니다.
+            
+            조건에 맞게 검색된 스터디 목록이 반환 됩니다.""",
+        security = @SecurityRequirement(name = "accessToken")
+    )
+    @Parameter(name = "userId", description = "접속한 유저의 ID를 입력 받습니다.", required = true)
+    public void interestRegionStudiesByTopic(
+        @PathVariable long userId
+    ) {
+        // 메소드 구현
+    }
+
+    @GetMapping("/search/users/{userId}/preferred-region/studies/conditions/all")
+    @Operation(
+        summary = "[내 관심 지역 스터디 조회 - 개발중] 특정 조건 + 내 '전체' 관심 지역 스터디 조회",
+        description = """
+            ## [내 관심 지역 스터디 조회] 입력한 조건에 맞는 회원의 전체 관심 지역의 스터디를 조회 합니다.
+            
+            조건에 맞게 검색된 스터디 목록이 반환 됩니다.""",
+        security = @SecurityRequirement(name = "accessToken")
+    )
+    @Parameter(name = "userId", description = "접속한 유저의 ID를 입력 받습니다.", required = true)
+    @Parameter(name = "gender", description = "성별을 입력 받습니다.", required = false)
+    @Parameter(name = "minAge", description = "minAge는 18 이상의 정수 입니다.", required = false)
+    @Parameter(name = "maxAge", description = "maxAge는 60 이하의 정수를 입력 받습니다.", required = false)
+    @Parameter(name = "isOnline", description = "스터디 온라인 진행 여부 입니다. true, false의 bool 값으로 입력 받습니다", required = false)
+    @Parameter(name = "hasFee", description = "스터디 활동비 유무 입니다. true, false의 bool 값으로 입력 받습니다", required = false)
+    public void interestRegionStudiesByConditionsAll(
+        @PathVariable long userId,
+        @RequestParam Gender gender,
+        @RequestParam int minAge,
+        @RequestParam int maxAge,
+        @RequestParam boolean isOnline,
+        @RequestParam boolean hasFee
+    ) {
+        // 메소드 구현
+    }
+
+    @GetMapping("/search/users/{userId}/preferred-region/studies/conditions/specific")
+    @Operation(
+        summary = "[내 관심 지역 스터디 조회 - 개발중] 특정 조건 + 내 '특정' 관심 지역 스터디 조회",
+        description = """
+            ## [내 관심 지역 스터디 조회] 입력한 조건에 맞는 회원의 특정 관심 지역의 스터디를 조회 합니다.
+            
+            조건에 맞게 검색된 스터디 목록이 반환 됩니다.""",
+        security = @SecurityRequirement(name = "accessToken")
+    )
+    @Parameter(name = "userId", description = "접속한 유저의 ID를 입력 받습니다.", required = true)
+    @Parameter(name = "gender", description = "성별을 입력 받습니다.", required = false)
+    @Parameter(name = "minAge", description = "minAge는 18 이상의 정수 입니다.", required = false)
+    @Parameter(name = "maxAge", description = "maxAge는 60 이하의 정수를 입력 받습니다.", required = false)
+    @Parameter(name = "isOnline", description = "스터디 온라인 진행 여부 입니다. true, false의 bool 값으로 입력 받습니다", required = false)
+    @Parameter(name = "hasFee", description = "스터디 활동비 유무 입니다. true, false의 bool 값으로 입력 받습니다", required = false)
+    public void interestRegionStudiesByConditionsSpecific(
+        @PathVariable long userId,
+        @RequestParam Gender gender,
+        @RequestParam int minAge,
+        @RequestParam int maxAge,
+        @RequestParam boolean isOnline,
+        @RequestParam boolean hasFee
+    ) {
+        // 메소드 구현
+    }
 
 
-
+    /* ----------------------------- 모집 중 스터디 조회  ------------------------------------- */
 }
