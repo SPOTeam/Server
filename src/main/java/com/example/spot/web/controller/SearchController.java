54ea7c1fb15f2e52bed7e3686ea522b34d4767be
msk226 <kminseok5167@gmail.com>
2024-07-17 13:54:07 +0900

    [docs] 관심 분야 스터디 조회 API 명세서 작성

--- src/main/java/com/example/spot/web/controller/SearchController.java
+++ src/main/java/com/example/spot/web/controller/SearchController.java
@@ -1,5 +1,6 @@
 package com.example.spot.web.controller;
 
+import com.example.spot.domain.enums.Gender;
 import com.example.spot.domain.enums.StudyState;
 import com.example.spot.domain.enums.ThemeType;
 import io.swagger.v3.oas.annotations.Operation;
@@ -23,6 +24,40 @@ public class SearchController {
         description = "접속한 회원의 추천 스터디 3개를 조회 합니다. Path variable로 접속한 유저의 ID를 입력 받습니다.",
         security = @SecurityRequirement(name = "accessToken"))
     public void recommendStudiesForMain(@PathVariable long userId) {}
+
+    @GetMapping("/search/interest/{userId}")
+    @Operation(summary = "[개발중] 회원 별 '전체' 관심 분야 스터디 조회",
+        description = "접속한 회원의 모든 관심 분야의 스터디를 전체 조회 합니다. Path variable로 접속한 유저의 ID를 입력 받습니다.",
+        security = @SecurityRequirement(name = "accessToken"))
+    public void interestStudies(@PathVariable long userId) {}
+
+    @GetMapping("/search/interest/{userId}")
+    @Operation(summary = "[개발중] 회원 별 '특정' 관심 분야 스터디 조회",
+        description = "접속한 회원의 특정 관심 분야의 스터디를 조회 합니다. Path variable로 접속한 유저의 ID를 입력 받습니다. "
+            + "Query parameter로 관심 분야를 입력 받습니다. "
+            + "themeType은 어학, 자격증, 취업, 시사뉴스, 자율학습, 토론, 프로젝트, 공모전, 전공및진로학습, 기타 중 선택합니다."
+            + "조건에 맞게 검색된 스터디 목록들이 반환 됩니다.",
+        security = @SecurityRequirement(name = "accessToken"))
+    public void interestStudiesByTopic(@PathVariable long userId, @RequestParam ThemeType themeType) {}
+
+    @GetMapping("/search/interest/{userId}")
+    @Operation(summary = "[개발중] 특정 조건 + 회원 별 관심 분야 스터디 조회",
+        description = "입력한 조건에 맞는 접속한 회원의 특정 관심 분야의 스터디를 조회 합니다. "
+            + "Path variable로 접속한 유저의 ID를 입력 받습니다. "
+            + "Query parameter로 성별, 최소 나이, 최대 나이, 온라인 여부, 유료 여부를 입력 받습니다. "
+            + "성별은 MALE, FEMALE, UNKNOWN으로 입력 받습니다. "
+            + "minAge 18 이상,, maxAge는 60 이하의 정수를 입력 받습니다."
+            + "isOnline과 hasFee은 true, false로 입력 받습니다."
+            + "조건에 맞게 검색된 스터디 목록들이 반환 됩니다.",
+        security = @SecurityRequirement(name = "accessToken"))
+    public void interestStudiesByConditions(@PathVariable long userId,
+                                            @RequestParam Gender gender,
+                                            @RequestParam int minAge,
+                                            @RequestParam int maxAge,
+                                            @RequestParam boolean isOnline,
+                                            @RequestParam boolean hasFee) {}
+
+
 
 
 
