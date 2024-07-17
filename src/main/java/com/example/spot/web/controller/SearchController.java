ecbe24ba724fa0b33be727e15d61029da052e005
msk226 <kminseok5167@gmail.com>
2024-07-17 13:42:29 +0900

    [docs] 메인 화면 용 관심, 추천 스터디 3개 조회 API 명세서 작성

--- src/main/java/com/example/spot/web/controller/SearchController.java
+++ src/main/java/com/example/spot/web/controller/SearchController.java
@@ -1,14 +1,31 @@
 package com.example.spot.web.controller;
 
 import com.example.spot.domain.enums.StudyState;
+import com.example.spot.domain.enums.ThemeType;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "Search", description = "Search API")
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/spot")
 public class SearchController {
+
+    @GetMapping("/search/interest/main/{userId}")
+    @Operation(summary = "[개발중] [메인 화면] 회원 관심 분야 스터디 3개 조회",
+        description = "접속한 회원의 관심 스터디 3개를 조회 합니다. Path variable로 접속한 유저의 ID를 입력 받습니다.",
+        security = @SecurityRequirement(name = "accessToken"))
+    public void interestStudiesForMain(@PathVariable long userId) {}
+
+    @GetMapping("/search/recommend/main/{userId}")
+    @Operation(summary = "[개발중] [메인 화면] 회원 별 추천 스터디 3개 조회",
+        description = "접속한 회원의 추천 스터디 3개를 조회 합니다. Path variable로 접속한 유저의 ID를 입력 받습니다.",
+        security = @SecurityRequirement(name = "accessToken"))
+    public void recommendStudiesForMain(@PathVariable long userId) {}
+
+
+
 
     @PostMapping("/search/recent-studies")
     @Operation(summary = "최근 검색 내역 저장하기", description = "회원의 최근 검색 내역을 임시 저장합니다.")
