6a78cba214321da419c26d50ef915aa8b124b169
kyoungmin <kkyoung0611@naver.com>
2024-07-19 00:14:51 +0900

    [fix] develop branch rebase

--- src/main/java/com/example/spot/web/controller/PostController.java
+++ src/main/java/com/example/spot/web/controller/PostController.java
@@ -1,11 +1,26 @@
 package com.example.spot.web.controller;
 
+import com.example.spot.web.dto.PostCreateRequest;
+import com.example.spot.web.service.PostService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "Post", description = "Post API")
 @RestController
-@RequestMapping("/api")
+@RequiredArgsConstructor
+@RequestMapping("/spot/api/posts")
 public class PostController {
+
+    private final PostService postService;
+
+    @Operation(summary = "게시글 등록 API", description = "form/data로 새로운 게시글을 생성합니다.")
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public void create(@ModelAttribute PostCreateRequest postCreateRequest) {
+        postService.createPost(postCreateRequest);
+        // ToDo 응답 통일한 후 반환 타입 수정
+    }
+
 }
---
+++ src/main/java/com/example/spot/web/service/PostService.java
@@ -0,0 +1,12 @@
+package com.example.spot.web.service;
+
+import com.example.spot.web.dto.PostCreateRequest;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostService {
+
+    public void createPost(PostCreateRequest postCreateRequest) {
+        // validate & save
+    }
+}
