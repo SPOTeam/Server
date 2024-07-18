acadba92971d6bca2db6806c314e6746e6109e46
kyoungmin <kkyoung0611@naver.com>
2024-07-18 22:52:56 +0900

    [docs] 게시글 Create API 명세서 작성

--- src/main/java/com/example/spot/controller/PostController.java
+++ src/main/java/com/example/spot/controller/PostController.java
@@ -1,11 +1,24 @@
 package com.example.spot.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "Post", description = "Post API")
 @RestController
-@RequestMapping("/api")
+@RequiredArgsConstructor
+@RequestMapping("/spot/api/posts")
 public class PostController {
+
+    private final PostService postService;
+
+    @Operation(summary = "게시글 등록 API", description = "form/data로 새로운 게시글을 생성합니다.")
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public void create(@ModelAttribute PostCreateRequest postCreateRequest) {
+        postService.createPost(postCreateRequest);
+        // ToDo 응답 통일한 후 반환 타입 수정
+    }
+
 }
---
+++ src/main/java/com/example/spot/controller/PostService.java
@@ -0,0 +1,11 @@
+package com.example.spot.controller;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostService {
+
+    public void createPost(PostCreateRequest postCreateRequest) {
+        // validate & save
+    }
+}
