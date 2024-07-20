8b49e70228a05f1ca38f2061f98594c7192bb67d
msk226 <kminseok5167@gmail.com>
2024-07-20 15:14:44 +0900

    [refactor] StudyQueryService에 작성한 메서드 리턴 타입 수정

--- src/main/java/com/example/spot/service/study/StudyQueryService.java
+++ src/main/java/com/example/spot/service/study/StudyQueryService.java
@@ -9,29 +9,29 @@ import org.springframework.data.domain.Pageable;
 public interface StudyQueryService {
 
     // 내 추천 스터디 조회
-    Page<Study> findRecommendStudies(Pageable pageable, Long memberId);
+    Page<SearchRequestDTO.SearchStudyDTO> findRecommendStudies(Pageable pageable, Long memberId);
 
     // 내 관심사 스터디 페이징 조회
-    Page<Study> findInterestStudiesByConditionsAll(Pageable pageable, Long memberId,
+    Page<SearchRequestDTO.SearchStudyDTO> findInterestStudiesByConditionsAll(Pageable pageable, Long memberId,
         SearchRequestDTO.SearchStudyDTO request);
 
     // 내 특정 관심사 스터디 페이징 조회
-    Page<Study> findInterestStudiesByConditionsSpecific(Pageable pageable, Long memberId,
+    Page<SearchRequestDTO.SearchStudyDTO> findInterestStudiesByConditionsSpecific(Pageable pageable, Long memberId,
         SearchRequestDTO.SearchStudyDTO request, Theme theme);
 
     // 내 관심 지역 스터디 페이징 조회
-    Page<Study> findInterestRegionStudiesByConditionsAll(Pageable pageable, Long memberId,
+    Page<SearchRequestDTO.SearchStudyDTO> findInterestRegionStudiesByConditionsAll(Pageable pageable, Long memberId,
         SearchRequestDTO.SearchStudyDTO request);
 
     // 내 특정 관심 지역 스터디 페이징 조회
-    Page<Study> findInterestRegionStudiesByConditionsSpecific(Pageable pageable, Long memberId,
+    Page<SearchRequestDTO.SearchStudyDTO> findInterestRegionStudiesByConditionsSpecific(Pageable pageable, Long memberId,
         SearchRequestDTO.SearchStudyDTO request, String regionCode);
 
     // 모집 중 스터디 조회
-    Page<Study> findRecruitingStudiesByConditions(Pageable pageable,
+    Page<SearchRequestDTO.SearchStudyDTO> findRecruitingStudiesByConditions(Pageable pageable,
         SearchRequestDTO.SearchStudyDTO request);
 
     // 찜한 스터디 조회
-    Page<Study> findLikedStudiesByConditions(Pageable pageable, Long memberId);
+    Page<SearchRequestDTO.SearchStudyDTO> findLikedStudiesByConditions(Pageable pageable, Long memberId);
 
 }
