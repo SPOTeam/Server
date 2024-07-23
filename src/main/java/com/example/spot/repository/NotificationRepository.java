56a1b1c6efb0fc5d0fd51d07d95bceced2e222aa
ISU <ourwavelets@gmail.com>
2024-07-23 23:46:18 +0900

    [fix] 패키지 위치 수정

--- src/main/java/com/example/spot/repository/NotificationRepository.java
+++ src/main/java/com/example/spot/repository/NotificationRepository.java
@@ -1,11 +1,15 @@
-package com.example.spot.repository.notification;
+package com.example.spot.repository;
 
+import com.example.spot.domain.enums.NotifyType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.spot.domain.Notification;
 
+import java.util.List;
+
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
-
+    List<Notification> findByMemberId(Long memberId);
+    List<Notification> findByType(NotifyType type);
 }
