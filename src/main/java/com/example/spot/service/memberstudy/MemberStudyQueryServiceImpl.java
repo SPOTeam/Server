a78afb337ace5e4e142147f95263134a71c54745
msk226 <kminseok5167@gmail.com>
2024-07-25 18:17:55 +0900

    [refactor] transaction 옵션 추가

--- src/main/java/com/example/spot/service/memberstudy/MemberStudyQueryServiceImpl.java
+++ src/main/java/com/example/spot/service/memberstudy/MemberStudyQueryServiceImpl.java
@@ -19,8 +19,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class MemberStudyQueryServiceImpl implements MemberStudyQueryService {
     private final StudyPostRepository studyPostRepository;
