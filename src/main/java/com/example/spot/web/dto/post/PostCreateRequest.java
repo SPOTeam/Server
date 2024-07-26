50b586092b3fcc9c602d5fd963fc0d94f10e76ed
kyoungmin <kkyoung0611@naver.com>
2024-07-27 03:24:24 +0900

    [correct] PostCreateRequest&PostUpdateRequest 수정

--- src/main/java/com/example/spot/web/dto/post/PostCreateRequest.java
+++ src/main/java/com/example/spot/web/dto/post/PostCreateRequest.java
@@ -4,19 +4,21 @@ import com.example.spot.domain.enums.Board;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class PostCreateRequest {
-    @Schema(
-            description = "게시글에 첨부할 이미지 파일 리스트. 파일 형식은 binary이며, " +
-                    "지원되는 이미지 형식은 JPEG, PNG입니다. 크기는 10MB 이하로만 가능합니다.",
-            format = "binary"
-    )
-    private List<MultipartFile> files;
+//    @Schema(
+//            description = "게시글에 첨부할 이미지 파일 리스트. 파일 형식은 binary이며, " +
+//                    "지원되는 이미지 형식은 JPEG, PNG입니다. 크기는 10MB 이하로만 가능합니다.",
+//            format = "binary"
+//    )
+//    private List<MultipartFile> files;
 
     @Schema(description = "게시글 제목입니다.",
             format = "string")
@@ -30,8 +32,8 @@ public class PostCreateRequest {
     private boolean isAnonymous;
 
     @Schema(
-            description = "게시글 타입입니다. 아래와 같이 작성해주세요.",
-            allowableValues = {"ALL", "PASS_EXPERIENCE", "INFORMATION_SHARING", "COUNSELING", "JOB_TALK", "FREE_TALK", "SPOT_ANNOUNCEMENT"}
+            description = "게시글 타입입니다. " +
+                    "PASS_EXPERIENCE, INFORMATION_SHARING, COUNSELING, JOB_TALK, FREE_TALK, SPOT_ANNOUNCEMENT 중에 작성해주세요."
     )
     private String type;
 
--- src/main/java/com/example/spot/web/dto/post/PostUpdateRequest.java
+++ src/main/java/com/example/spot/web/dto/post/PostUpdateRequest.java
@@ -4,11 +4,13 @@ import com.example.spot.domain.enums.Board;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class PostUpdateRequest {
     @Schema(
