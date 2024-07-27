cd3b6b19e77e94f98d5dbc385a3860453ca7c1e1
dvlp-sy <sy8389b@gmail.com>
2024-07-27 15:37:04 +0900

    [add] Add StudyQuizDTOS

--- src/main/java/com/example/spot/web/dto/memberstudy/response/StudyQuizResponseDTO.java
+++ src/main/java/com/example/spot/web/dto/memberstudy/response/StudyQuizResponseDTO.java
@@ -1,10 +1,15 @@
 package com.example.spot.web.dto.memberstudy.response;
 
+import com.example.spot.domain.Member;
 import com.example.spot.domain.Quiz;
+import com.example.spot.domain.mapping.MemberAttendance;
+import com.example.spot.domain.study.Study;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Getter
 public class StudyQuizResponseDTO {
@@ -21,6 +26,28 @@ public class StudyQuizResponseDTO {
             return QuizDTO.builder()
                     .quizId(quiz.getId())
                     .question(quiz.getQuestion())
+                    .build();
+        }
+    }
+
+    @Getter
+    @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+    @Builder(access = AccessLevel.PRIVATE)
+    public static class AttendanceDTO {
+
+        private final Long memberId;
+        private final Long quizId;
+        private final Long attendanceId;
+        private final Boolean isCorrect;
+        private final LocalDateTime createdAt;
+
+        public static AttendanceDTO toDTO(MemberAttendance memberAttendance) {
+            return AttendanceDTO.builder()
+                    .memberId(memberAttendance.getMember().getId())
+                    .quizId(memberAttendance.getQuiz().getId())
+                    .attendanceId(memberAttendance.getId())
+                    .isCorrect(memberAttendance.getIsCorrect())
+                    .createdAt(memberAttendance.getCreatedAt())
                     .build();
         }
     }
