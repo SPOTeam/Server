b01e2d2f61266a80c63618d7173b87a20a45b899
dvlp-sy <sy8389b@gmail.com>
2024-07-27 15:36:48 +0900

    [add] Add Success and Error Status

--- src/main/java/com/example/spot/api/code/status/ErrorStatus.java
+++ src/main/java/com/example/spot/api/code/status/ErrorStatus.java
@@ -62,7 +62,14 @@ public enum ErrorStatus implements BaseErrorCode {
     _STUDY_SORT_BY_NOT_FOUND(HttpStatus.NOT_FOUND, "STUDY6007", "스터디 정렬 기준을 찾을 수 없습니다."),
     _STUDY_ID_NULL(HttpStatus.BAD_REQUEST, "STUDY6008", "스터디 아이디가 입력되지 않았습니다."),
     _STUDY_REGION_IS_NOT_MATCH(HttpStatus.BAD_REQUEST, "STUDY6009", "입력한 회원의 관심 스터디 지역을 입력하세요."),
-    _STUDY_IS_NOT_MATCH(HttpStatus.BAD_REQUEST, "STUDY6010", "입력한 조건에 맞는 스터디가 존재하지 않습니다.");
+    _STUDY_IS_NOT_MATCH(HttpStatus.BAD_REQUEST, "STUDY6010", "입력한 조건에 맞는 스터디가 존재하지 않습니다."),
+
+    // 스터디 출석 관련 에러
+    _STUDY_QUIZ_NOT_FOUND(HttpStatus.NOT_FOUND, "QUIZ4001", "출석 퀴즈를 찾을 수 없습니다."),
+    _STUDY_QUIZ_NOT_VALID(HttpStatus.BAD_REQUEST, "QUIZ4002", "출석 퀴즈의 제한 시간이 초과되었습니다."),
+
+    ;
+
 
 
 
