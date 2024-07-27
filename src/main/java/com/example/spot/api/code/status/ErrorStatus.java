23827e7db72490ce09ae930af13cb30d08a32a84
msk226 <kminseok5167@gmail.com>
2024-07-27 14:51:25 +0900

    [fix] build 실패 에러 해결

--- src/main/java/com/example/spot/api/code/status/ErrorStatus.java
+++ src/main/java/com/example/spot/api/code/status/ErrorStatus.java
@@ -62,7 +62,7 @@ public enum ErrorStatus implements BaseErrorCode {
     _STUDY_SORT_BY_NOT_FOUND(HttpStatus.NOT_FOUND, "STUDY6007", "스터디 정렬 기준을 찾을 수 없습니다."),
     _STUDY_ID_NULL(HttpStatus.BAD_REQUEST, "STUDY6008", "스터디 아이디가 입력되지 않았습니다."),
     _STUDY_REGION_IS_NOT_MATCH(HttpStatus.BAD_REQUEST, "STUDY6009", "입력한 회원의 관심 스터디 지역을 입력하세요."),
-    _STUDY_IS_NOT_MATCH(HttpStatus.BAD_REQUEST, "STUDY6010", "입력한 조건에 맞는 스터디가 존재하지 않습니다.");
+    _STUDY_IS_NOT_MATCH(HttpStatus.BAD_REQUEST, "STUDY6010", "입력한 조건에 맞는 스터디가 존재하지 않습니다."),
 
 
     //게시글 관련 에러
@@ -71,8 +71,7 @@ public enum ErrorStatus implements BaseErrorCode {
     _POST_CONTENT_INVALID(HttpStatus.BAD_REQUEST, "POST5003", "게시글 내용이 유효하지 않습니다."),
     _POST_NOT_AUTHOR(HttpStatus.BAD_REQUEST, "POST5004", "게시글 작성자가 아닙니다."),
     _POST_REPORTED(HttpStatus.FORBIDDEN, "POST5005", "신고된 게시글입니다."),
-    _INVALID_BOARD_TYPE(HttpStatus.BAD_REQUEST, "POST5006", "유효하지 않은 게시판 타입입니다."), // 추가된 에러
-    ;
+    _INVALID_BOARD_TYPE(HttpStatus.BAD_REQUEST, "POST5006", "유효하지 않은 게시판 타입입니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
