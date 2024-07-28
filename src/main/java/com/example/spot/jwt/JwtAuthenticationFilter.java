4a44f32228bceca7b6183898bdb98f7882b95bcd
ISU <ourwavelets@gmail.com>
2024-07-29 07:50:31 +0900

    [fix] 오타 수정

--- src/main/java/com/example/spot/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/example/spot/jwt/JwtAuthenticationFilter.java
@@ -47,8 +47,10 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private String getJwtFromRequest(HttpServletRequest request) {
         String bearerToken = request.getHeader("Authorization");
         if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
-            return bearerToken.substring("Baerer ".length());
+            return bearerToken.substring("Bearer ".length());
         }
         return null;
     }
+
+    //bearer
 }
