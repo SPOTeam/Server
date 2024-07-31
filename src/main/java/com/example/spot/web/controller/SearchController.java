9cb84b530c64d18cff38728c599ccc8f3cdacc5f
msk226 <kminseok5167@gmail.com>
2024-07-31 14:27:28 +0900

    [refactor] 스웨거 명세 수정

--- src/main/java/com/example/spot/web/controller/SearchController.java
+++ src/main/java/com/example/spot/web/controller/SearchController.java
@@ -46,8 +46,8 @@ public class SearchController {
     @GetMapping("/search/studies/my-page/members/{memberId}")
     @Operation(summary = "[마이 페이지] 마이 페이지 내 스터디 정보 조회",
         description = """
-            ## [마이 페이지] 마이 페이지 나와 관련된 스터디 갯수 정보를 조회합니다.
-            스터디 갯수 정보가 반환 됩니다.""",
+            ## [마이 페이지] 마이 페이지에 들어갈 나와 관련된 스터디 갯수 정보를 조회합니다.
+            스터디 갯수 정보와 내 이름이 반환 됩니다.""",
         security = @SecurityRequirement(name = "accessToken"))
     @Parameter(name = "memberId", description = "조회할 유저의 ID를 입력 받습니다.", required = true)
     public ApiResponse<MyPageDTO> myPage(@PathVariable @ExistMember long memberId) {
