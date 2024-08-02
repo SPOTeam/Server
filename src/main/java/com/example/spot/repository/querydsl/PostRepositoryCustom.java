59f05230cd2d55bb8d68bd08fb86319c8dde61c6
kyoungmin <kkyoung0611@naver.com>
2024-08-03 05:19:02 +0900

    [feat] 타입별 대표 게시글 조회 기능 구현

--- src/main/java/com/example/spot/repository/querydsl/PostRepositoryCustom.java
+++ src/main/java/com/example/spot/repository/querydsl/PostRepositoryCustom.java
@@ -14,4 +14,8 @@ public interface PostRepositoryCustom {
 
     // 좋아요 수, 조회수, 댓글 수를 합산하여 가장 높은 다섯개의 게시글을 가져오는 쿼리 메서드
     List<Post> findTopByRealTimeScore();
+
+    //게시글 타입별 최신 게시글 출력
+    List<Post> findRepresentativePosts();
+
 }
--- src/main/java/com/example/spot/service/post/PostQueryService.java
+++ src/main/java/com/example/spot/service/post/PostQueryService.java
@@ -2,6 +2,7 @@ package com.example.spot.service.post;
 
 import com.example.spot.web.dto.post.PostBest5Response;
 import com.example.spot.web.dto.post.PostPagingResponse;
+import com.example.spot.web.dto.post.PostRepresentativeResponse;
 import com.example.spot.web.dto.post.PostSingleResponse;
 import org.springframework.data.domain.Pageable;
 
@@ -11,5 +12,7 @@ public interface PostQueryService {
     PostPagingResponse getPagingPosts(String type, Pageable pageable);
 
     PostBest5Response getPostBest(String sortType);
+
+    PostRepresentativeResponse getRepresentativePosts();
 
 }
