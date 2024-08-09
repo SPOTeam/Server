03d0fcc15d48e0e4494acc18c376a34739b88584
kyoungmin <kkyoung0611@naver.com>
2024-08-10 03:35:49 +0900

    [feat] 게시글 대댓글 생성 기능 구현

--- src/main/java/com/example/spot/repository/PostCommentRepository.java
+++ src/main/java/com/example/spot/repository/PostCommentRepository.java
@@ -3,5 +3,7 @@ package com.example.spot.repository;
 import com.example.spot.domain.PostComment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 public interface PostCommentRepository extends JpaRepository<PostComment, Long> {
+    List<PostComment> findByPostId(Long postId);
 }
