403ae4f213b687823eaa7b7310e58679f81e51f0
msk226 <kminseok5167@gmail.com>
2024-08-10 14:00:11 +0900

    [feat] 특정 지역/분야 및 전체 지역/분야 스터디 조회 테스트 코드 작성

--- src/test/java/com/example/spot/service/study/StudyQueryServiceTest.java
+++ src/test/java/com/example/spot/service/study/StudyQueryServiceTest.java
@@ -7,10 +7,14 @@ import static org.mockito.Mockito.when;
 import static org.mockito.internal.verification.VerificationModeFactory.times;
 
 import com.example.spot.domain.Member;
+import com.example.spot.domain.Region;
 import com.example.spot.domain.Theme;
 import com.example.spot.domain.enums.Gender;
+import com.example.spot.domain.enums.StudySortBy;
 import com.example.spot.domain.enums.ThemeType;
 import com.example.spot.domain.mapping.MemberTheme;
+import com.example.spot.domain.mapping.PreferredRegion;
+import com.example.spot.domain.mapping.RegionStudy;
 import com.example.spot.domain.mapping.StudyTheme;
 import com.example.spot.domain.study.Study;
 import com.example.spot.repository.MemberRepository;
@@ -22,17 +26,26 @@ import com.example.spot.repository.RegionStudyRepository;
 import com.example.spot.repository.StudyRepository;
 import com.example.spot.repository.StudyThemeRepository;
 import com.example.spot.repository.ThemeRepository;
+import com.example.spot.web.dto.search.SearchRequestDTO.SearchRequestStudyDTO;
 import com.example.spot.web.dto.search.SearchResponseDTO.StudyPreviewDTO;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 @ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
 class StudyQueryServiceTest {
 
     @Mock
@@ -117,6 +130,7 @@ class StudyQueryServiceTest {
     }
 
     @Test
+    @DisplayName("추천 스터디 조회 - 추천 스터디가 있는 경우")
     void findRecommendStudies() {
         // given
         Member member = Member.builder()
@@ -164,19 +178,303 @@ class StudyQueryServiceTest {
     }
 
     @Test
+    @DisplayName("내 전체 관심사 조회 - 내 전체 관심사에 해당하는 스터디가 있는 경우")
     void findInterestStudiesByConditionsAll() {
+        // given
+        Member member = Member.builder()
+            .id(1L)
+            .build();
+
+        Pageable pageable = PageRequest.of(0, 10);
+
+        Theme theme1 = Theme.builder()
+            .id(1L)
+            .studyTheme(ThemeType.어학)
+            .build();
+
+        Theme theme2 = Theme.builder()
+            .id(2L)
+            .studyTheme(ThemeType.공모전)
+            .build();
+
+        StudySortBy sortBy = StudySortBy.ALL;
+
+        SearchRequestStudyDTO request = SearchRequestStudyDTO.builder()
+            .gender(Gender.MALE)
+            .minAge(20)
+            .maxAge(40)
+            .fee(10000)
+            .isOnline(true)
+            .hasFee(true)
+            .build();
+
+        MemberTheme memberTheme1 = MemberTheme.builder().member(member).theme(theme1).build();
+        MemberTheme memberTheme2 = MemberTheme.builder().member(member).theme(theme2).build();
+
+        StudyTheme studyTheme1 = new StudyTheme(theme1, study1);
+        StudyTheme studyTheme2 = new StudyTheme(theme2, study2);
+
+        // Mock conditions
+        Map<String, Object> searchConditions = new HashMap<>();
+        searchConditions.put("gender", Gender.MALE);
+        searchConditions.put("minAge", 20);
+        searchConditions.put("maxAge", 40);
+        searchConditions.put("isOnline", true);
+        searchConditions.put("hasFee", true);
+        searchConditions.put("fee", 10000);
+
+        when(memberThemeRepository.findAllByMemberId(member.getId()))
+            .thenReturn(List.of(memberTheme1, memberTheme2));
+        when(studyThemeRepository.findAllByTheme(theme1))
+            .thenReturn(List.of(studyTheme1));
+        when(studyThemeRepository.findAllByTheme(theme2))
+            .thenReturn(List.of(studyTheme2));
+
+        when(studyRepository.countStudyByConditionsAndThemeTypes(searchConditions, List.of(studyTheme1, studyTheme2), sortBy))
+            .thenReturn(2L);
+
+        when(studyRepository.findStudyByConditionsAndThemeTypes(
+            searchConditions, sortBy, pageable, List.of(studyTheme1, studyTheme2)))
+            .thenReturn(List.of(study1, study2));
+
+        // when
+        StudyPreviewDTO result = studyQueryService.findInterestStudiesByConditionsAll(pageable, member.getId(), request, sortBy);
+
+        // then
+        assertNotNull(result);
+        assertEquals(2, result.getTotalElements());  // Assuming StudyPreviewDTO has a getSize method
+        verify(memberThemeRepository).findAllByMemberId(member.getId());
+        verify(studyThemeRepository, times(1)).findAllByTheme(theme1);
+        verify(studyThemeRepository, times(1)).findAllByTheme(theme2);
+        verify(studyRepository).countStudyByConditionsAndThemeTypes(searchConditions, List.of(studyTheme1, studyTheme2), sortBy);
+        verify(studyRepository).findStudyByConditionsAndThemeTypes(searchConditions, sortBy, pageable, List.of(studyTheme1, studyTheme2));
+
     }
 
     @Test
+    @DisplayName("내 특정 관심사 조회 - 내 특정 관심사에 해당하는 스터디가 있는 경우")
     void findInterestStudiesByConditionsSpecific() {
+        // given
+        Member member = Member.builder()
+            .id(1L)
+            .build();
+
+        Pageable pageable = PageRequest.of(0, 10);
+
+        ThemeType themeType = ThemeType.어학;
+
+        Theme theme1 = Theme.builder()
+            .id(1L)
+            .studyTheme(ThemeType.어학)
+            .build();
+
+        Theme theme2 = Theme.builder()
+            .id(2L)
+            .studyTheme(ThemeType.공모전)
+            .build();
+
+        StudySortBy sortBy = StudySortBy.ALL;
+
+        SearchRequestStudyDTO request = SearchRequestStudyDTO.builder()
+            .gender(Gender.MALE)
+            .minAge(20)
+            .maxAge(40)
+            .fee(10000)
+            .isOnline(true)
+            .hasFee(true)
+            .build();
+
+        MemberTheme memberTheme1 = MemberTheme.builder().member(member).theme(theme1).build();
+        MemberTheme memberTheme2 = MemberTheme.builder().member(member).theme(theme2).build();
+
+        StudyTheme studyTheme1 = new StudyTheme(theme1, study1);
+        StudyTheme studyTheme2 = new StudyTheme(theme2, study2);
+
+        // Mock conditions
+        Map<String, Object> searchConditions = new HashMap<>();
+        searchConditions.put("gender", Gender.MALE);
+        searchConditions.put("minAge", 20);
+        searchConditions.put("maxAge", 40);
+        searchConditions.put("isOnline", true);
+        searchConditions.put("hasFee", true);
+        searchConditions.put("fee", 10000);
+
+        when(memberThemeRepository.findAllByMemberId(member.getId()))
+            .thenReturn(List.of(memberTheme1));
+        when(studyThemeRepository.findAllByTheme(theme1))
+            .thenReturn(List.of(studyTheme1));
+
+        // Only studyTheme1 should match
+        when(studyRepository.countStudyByConditionsAndThemeTypes(searchConditions, List.of(studyTheme1), sortBy))
+            .thenReturn(1L);
+
+        // Adjusting the mock to match the specific test data
+        when(studyRepository.findStudyByConditionsAndThemeTypes(
+            searchConditions, sortBy, pageable, List.of(studyTheme1)))
+            .thenReturn(List.of(study1));
+
+        // when
+        StudyPreviewDTO result = studyQueryService.findInterestStudiesByConditionsSpecific(pageable, member.getId(), request, themeType, sortBy);
+
+        // then
+        assertNotNull(result);
+        assertEquals(1, result.getTotalElements());  // Verify the count matches expected result
+        verify(memberThemeRepository).findAllByMemberId(member.getId());
+        verify(studyThemeRepository).findAllByTheme(theme1);  // Ensure the correct theme is queried
+        verify(studyRepository).countStudyByConditionsAndThemeTypes(searchConditions, List.of(studyTheme1), sortBy);
+        verify(studyRepository).findStudyByConditionsAndThemeTypes(searchConditions, sortBy, pageable, List.of(studyTheme1));
     }
 
     @Test
     void findInterestRegionStudiesByConditionsAll() {
+        // given
+        Member member = Member.builder()
+            .id(1L)
+            .build();
+
+        Pageable pageable = PageRequest.of(0, 10);
+
+        Region region1 = Region.builder()
+            .province("경기도")
+            .neighborhood("송산면")
+            .district("화성시")
+            .code("4159034000")
+            .build();
+
+        Region region2 = Region.builder()
+            .province("경기도")
+            .neighborhood("봉담읍")
+            .district("화성시")
+            .code("4159025300")
+            .build();
+
+        StudySortBy sortBy = StudySortBy.ALL;
+
+        SearchRequestStudyDTO request = SearchRequestStudyDTO.builder()
+            .gender(Gender.MALE)
+            .minAge(20)
+            .maxAge(40)
+            .fee(10000)
+            .isOnline(true)
+            .hasFee(true)
+            .build();
+
+        PreferredRegion preferredRegion1 = PreferredRegion.builder().member(member).region(region1).build();
+        PreferredRegion preferredRegion2 = PreferredRegion.builder().member(member).region(region2).build();
+
+        RegionStudy regionStudy1 = RegionStudy.builder().region(region1).study(study1).build();
+        RegionStudy regionStudy2 = RegionStudy.builder().region(region2).study(study2).build();
+
+        // Mock conditions
+        Map<String, Object> searchConditions = new HashMap<>();
+        searchConditions.put("gender", Gender.MALE);
+        searchConditions.put("minAge", 20);
+        searchConditions.put("maxAge", 40);
+        searchConditions.put("isOnline", true);
+        searchConditions.put("hasFee", true);
+        searchConditions.put("fee", 10000);
+
+        when(preferredRegionRepository.findAllByMemberId(member.getId()))
+            .thenReturn(List.of(preferredRegion1, preferredRegion2));
+        when(regionStudyRepository.findAllByRegion(region1))
+            .thenReturn(List.of(regionStudy1));
+        when(regionStudyRepository.findAllByRegion(region2))
+            .thenReturn(List.of(regionStudy2));
+
+        when(studyRepository.countStudyByConditionsAndRegionStudies(searchConditions, List.of(regionStudy1, regionStudy2), sortBy))
+            .thenReturn(2L);
+
+        when(studyRepository.findStudyByConditionsAndRegionStudies(
+            searchConditions, sortBy, pageable, List.of(regionStudy1, regionStudy2)))
+            .thenReturn(List.of(study1, study2));
+
+        // when
+        StudyPreviewDTO result = studyQueryService.findInterestRegionStudiesByConditionsAll(pageable, member.getId(), request, sortBy);
+
+        // then
+        assertNotNull(result);
+        assertEquals(2, result.getTotalElements());  // Assuming StudyPreviewDTO has a getSize method
+        verify(preferredRegionRepository).findAllByMemberId(member.getId());
+        verify(regionStudyRepository, times(1)).findAllByRegion(region1);
+        verify(regionStudyRepository, times(1)).findAllByRegion(region2);
+        verify(studyRepository).countStudyByConditionsAndRegionStudies(searchConditions, List.of(regionStudy1, regionStudy2), sortBy);
+        verify(studyRepository).findStudyByConditionsAndRegionStudies(searchConditions, sortBy, pageable, List.of(regionStudy1, regionStudy2));
     }
 
     @Test
     void findInterestRegionStudiesByConditionsSpecific() {
+        // given
+        Member member = Member.builder()
+            .id(1L)
+            .build();
+
+        String regionCode = "4159034000";
+
+        Pageable pageable = PageRequest.of(0, 10);
+
+        Region region1 = Region.builder()
+            .province("경기도")
+            .neighborhood("송산면")
+            .district("화성시")
+            .code("4159034000")
+            .build();
+
+        Region region2 = Region.builder()
+            .province("경기도")
+            .neighborhood("봉담읍")
+            .district("화성시")
+            .code("4159025300")
+            .build();
+
+        StudySortBy sortBy = StudySortBy.ALL;
+
+        SearchRequestStudyDTO request = SearchRequestStudyDTO.builder()
+            .gender(Gender.MALE)
+            .minAge(20)
+            .maxAge(40)
+            .fee(10000)
+            .isOnline(true)
+            .hasFee(true)
+            .build();
+        PreferredRegion preferredRegion1 = PreferredRegion.builder().member(member).region(region1).build();
+        PreferredRegion preferredRegion2 = PreferredRegion.builder().member(member).region(region2).build();
+
+        RegionStudy regionStudy1 = RegionStudy.builder().region(region1).study(study1).build();
+        RegionStudy regionStudy2 = RegionStudy.builder().region(region2).study(study2).build();
+
+        // Mock conditions
+        Map<String, Object> searchConditions = new HashMap<>();
+        searchConditions.put("gender", Gender.MALE);
+        searchConditions.put("minAge", 20);
+        searchConditions.put("maxAge", 40);
+        searchConditions.put("isOnline", true);
+        searchConditions.put("hasFee", true);
+        searchConditions.put("fee", 10000);
+
+        when(preferredRegionRepository.findAllByMemberId(member.getId()))
+            .thenReturn(List.of(preferredRegion1));
+        when(regionStudyRepository.findAllByRegion(region1))
+            .thenReturn(List.of(regionStudy1));
+
+        // Only studyTheme1 should match
+        when(studyRepository.countStudyByConditionsAndRegionStudies(searchConditions, List.of(regionStudy1), sortBy))
+            .thenReturn(1L);
+
+        // Adjusting the mock to match the specific test data
+        when(studyRepository.findStudyByConditionsAndRegionStudies(
+            searchConditions, sortBy, pageable, List.of(regionStudy1)))
+            .thenReturn(List.of(study1));
+
+        // when
+        StudyPreviewDTO result = studyQueryService.findInterestRegionStudiesByConditionsSpecific(pageable, member.getId(), request, regionCode, sortBy);
+
+        // then
+        assertNotNull(result);
+        assertEquals(1, result.getTotalElements());  // Verify the count matches expected result
+        verify(preferredRegionRepository).findAllByMemberId(member.getId());
+        verify(regionStudyRepository).findAllByRegion(region1);  // Ensure the correct theme is queried
+        verify(studyRepository).countStudyByConditionsAndRegionStudies(searchConditions, List.of(regionStudy1), sortBy);
+        verify(studyRepository).findStudyByConditionsAndRegionStudies(searchConditions, sortBy, pageable, List.of(regionStudy1));
     }
 
     @Test
