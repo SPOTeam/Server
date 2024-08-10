6332a15546e0726f0713f5c8764460e0444b8d4d
msk226 <kminseok5167@gmail.com>
2024-08-10 12:42:23 +0900

    [feat] 테스트 코드 클래스 작성

---
+++ src/test/java/com/example/spot/service/study/StudyCommandServiceTest.java
@@ -0,0 +1,20 @@
+package com.example.spot.service.study;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class StudyCommandServiceTest {
+
+    @Test
+    void applyToStudy() {
+    }
+
+    @Test
+    void registerStudy() {
+    }
+
+    @Test
+    void likeStudy() {
+    }
+}
---
+++ src/test/java/com/example/spot/service/study/StudyQueryServiceTest.java
@@ -0,0 +1,64 @@
+package com.example.spot.service.study;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class StudyQueryServiceTest {
+
+    @Test
+    void getStudyInfo() {
+    }
+
+    @Test
+    void getMyPageStudyCount() {
+    }
+
+    @Test
+    void findRecommendStudies() {
+    }
+
+    @Test
+    void findInterestStudiesByConditionsAll() {
+    }
+
+    @Test
+    void findInterestStudiesByConditionsSpecific() {
+    }
+
+    @Test
+    void findInterestRegionStudiesByConditionsAll() {
+    }
+
+    @Test
+    void findInterestRegionStudiesByConditionsSpecific() {
+    }
+
+    @Test
+    void findRecruitingStudiesByConditions() {
+    }
+
+    @Test
+    void findLikedStudies() {
+    }
+
+    @Test
+    void findStudiesByKeyword() {
+    }
+
+    @Test
+    void findStudiesByTheme() {
+    }
+
+    @Test
+    void findOngoingStudiesByMemberId() {
+    }
+
+    @Test
+    void findAppliedStudies() {
+    }
+
+    @Test
+    void findMyRecruitingStudies() {
+    }
+}
