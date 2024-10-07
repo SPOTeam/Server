95f96e751a73e285dd9d2c312e22c7e6749b1b01
msk226 <kminseok5167@gmail.com>
2024-10-07 09:45:30 +0900

    [SPOT-55][feat] RedisConfig 내 RedisTemplate 메서드 추가

--- src/main/java/com/example/spot/config/RedisConfig.java
+++ src/main/java/com/example/spot/config/RedisConfig.java
@@ -8,7 +8,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 @RequiredArgsConstructor
@@ -28,6 +30,33 @@ public class RedisConfig extends CachingConfigurerSupport {
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
         return new LettuceConnectionFactory(host, port);
+    }
+
+    /**
+     * Redis 데이터 처리를 위한 템플릿을 구성합니다.
+     *  RedisTemplate을 통해서 데이터를 읽고 쓸 때 Java 객체와 Redis의 문자열 형식 간의 변환을 수행합니다.
+     * Java와 Redis 간의 데이터 포맷 차이를 신경 쓰지 않고 편리하게 데이터 통신을 할 수 있습니다.
+     * @return RedisTemplate<String, Object>
+     */
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+
+        // Redis를 연결합니다.
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+
+        // Key-Value 형태로 직렬화를 수행합니다.
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+
+        // Hash Key-Value 형태로 직렬화를 수행합니다.
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new StringRedisSerializer());
+
+        // 기본적으로 직렬화를 수행합니다.
+        redisTemplate.setDefaultSerializer(new StringRedisSerializer());
+
+        return redisTemplate;
     }
 
 
