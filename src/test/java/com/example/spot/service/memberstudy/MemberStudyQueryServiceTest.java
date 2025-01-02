c9e423b9ef1bcfbf34128ea95c24b4194c2d1fc8
msk226 <kminseok5167@gmail.com>
2025-01-02 18:30:41 +0900

    [SPOT-134][TEST] 스터디에 다가오는 모임 목록 조회 테스트 코드 작성

--- src/test/java/com/example/spot/service/memberstudy/MemberStudyQueryServiceTest.java
+++ src/test/java/com/example/spot/service/memberstudy/MemberStudyQueryServiceTest.java
@@ -5,14 +5,18 @@ import com.example.spot.domain.Member;
 import com.example.spot.domain.enums.ApplicationStatus;
 import com.example.spot.domain.enums.Theme;
 import com.example.spot.domain.mapping.MemberStudy;
+import com.example.spot.domain.study.Schedule;
 import com.example.spot.domain.study.Study;
 import com.example.spot.domain.study.StudyPost;
 import com.example.spot.repository.MemberRepository;
 import com.example.spot.repository.MemberStudyRepository;
+import com.example.spot.repository.ScheduleRepository;
 import com.example.spot.repository.StudyPostRepository;
 import com.example.spot.security.utils.SecurityUtils;
 import com.example.spot.web.dto.study.response.StudyPostResponseDTO;
+import com.example.spot.web.dto.study.response.StudyScheduleResponseDTO;
 import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -31,6 +35,7 @@ import static org.mockito.Mockito.when;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
@@ -50,11 +55,13 @@ public class MemberStudyQueryServiceTest {
     @Mock
     private StudyPostRepository studyPostRepository;
     @Mock
+    private ScheduleRepository scheduleRepository;
+    @Mock
     private SecurityUtils securityUtils;
 
     private static Member member;
     private static Study study;
-
+    private static MemberStudy memberStudy;
     @BeforeEach
     void setup(){
         member = Member.builder()
@@ -64,6 +71,13 @@ public class MemberStudyQueryServiceTest {
         study = Study.builder()
                 .build();
 
+        memberStudy = MemberStudy.builder()
+                .introduction("title").study(study).member(member).isOwned(true).status(ApplicationStatus.APPROVED).build();
+
+        Long studyId = 1L;
+
+        when(memberStudyRepository.findByMemberIdAndStudyIdAndStatus(1L, studyId, ApplicationStatus.APPROVED)).thenReturn(
+                Optional.ofNullable(memberStudy));
         Authentication authentication = new UsernamePasswordAuthenticationToken("1", null, Collections.emptyList());
         // SecurityContext 생성 및 설정
         SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
@@ -82,7 +96,8 @@ public class MemberStudyQueryServiceTest {
 
         // then
     }
-/* ------------------------------------------------ 스터디 상세 정보  --------------------------------------------------- */
+
+    /* ------------------------------------------------ 스터디 공지사항 조회  --------------------------------------------------- */
 
     @Test
     @DisplayName("스터디 공지사항 조회 - 성공")
@@ -139,5 +154,50 @@ public class MemberStudyQueryServiceTest {
         assertThrows(GeneralException.class, () -> memberStudyQueryService.findStudyAnnouncementPost(studyId));
     }
 
+    /* ------------------------------------------------ 스터디 모임 목록 조회  --------------------------------------------------- */
+
+    @Test
+    @DisplayName("스터디 모임 목록 조회 - 성공")
+    void 스터디_모임_목록_조회_성공(){
+        // given
+        Long studyId = 1L;
+        String title = "title";
+
+        Schedule schedule1 = Schedule.builder().id(1L).title(title).build();
+        Schedule schedule2 = Schedule.builder().id(2L).title("title1").build();
+        when(scheduleRepository.findAllByStudyId(studyId, Pageable.unpaged())).thenReturn(List.of(schedule1, schedule2));
+
+        // when
+        StudyScheduleResponseDTO responseDTO = memberStudyQueryService.findStudySchedule(studyId, Pageable.unpaged());
+
+        // then
+        assertEquals(2, responseDTO.getTotalElements());
+        assertEquals(title, responseDTO.getSchedules().get(0).getTitle());
+    }
+
+    @Test
+    @DisplayName("스터디 모임 목록 조회 - 로그인 한 회원이 해당 스터디 회원이 아닌 경우")
+    void 스터디_모임_목록_조회_실패_1(){
+        // given
+        long studyId = 1L;
+        when(memberStudyRepository.findByMemberIdAndStudyIdAndStatus(1L, studyId, ApplicationStatus.APPROVED)).thenReturn(
+                Optional.empty());
+
+        // when & then
+        assertThrows(GeneralException.class, () -> memberStudyQueryService.findStudySchedule(studyId, Pageable.unpaged()));
+    }
+
+    @Test
+    @DisplayName("스터디 모임 목록 조회 - 스터디 모임 일정이 존재하지 않는 경우")
+    void 스터디_모임_목록_조회_실패_2(){
+        // given
+        long studyId = 1L;
+        when(memberStudyRepository.findByMemberIdAndStudyIdAndStatus(1L, studyId, ApplicationStatus.APPROVED)).thenReturn(
+                Optional.of(memberStudy));
+        when(scheduleRepository.findAllByStudyId(studyId, Pageable.unpaged())).thenReturn(Collections.emptyList());
+
+        // when & then
+        assertThrows(GeneralException.class, () -> memberStudyQueryService.findStudySchedule(studyId, Pageable.unpaged()));
+    }
 
 }
