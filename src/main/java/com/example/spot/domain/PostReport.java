44842c227459a0cca6c1ca494d937ca069ef9637
msk226 <kminseok5167@gmail.com>
2025-03-16 22:57:04 +0900

    [SPOT-223][FEAT] PostReport 빌더 애노테이션 추가

--- src/main/java/com/example/spot/domain/PostReport.java
+++ src/main/java/com/example/spot/domain/PostReport.java
@@ -6,11 +6,15 @@ import com.example.spot.domain.common.BaseEntity;
 import com.example.spot.domain.enums.PostStatus;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@Builder
+@AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class PostReport extends BaseEntity {
     @Id
