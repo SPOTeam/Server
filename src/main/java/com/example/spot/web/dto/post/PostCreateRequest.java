c28ad379c89d8767695fb0cd4972ddcc7de756f4
dvlp-sy <sy8389b@gmail.com>
2025-04-03 00:10:16 +0900

    [SPOT-155][CHORE] Add Builder Annotation

--- src/main/java/com/example/spot/web/dto/post/PostCreateRequest.java
+++ src/main/java/com/example/spot/web/dto/post/PostCreateRequest.java
@@ -4,10 +4,12 @@ import com.example.spot.domain.enums.Board;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class PostCreateRequest {
