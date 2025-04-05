009d8a619f004e547dc8744dfea6a2af39c44d11
msk226 <kminseok5167@gmail.com>
2025-04-05 14:21:07 +0900

    [SPOT-253][MODIFY] 게시글 조회 시, 최신 게시글이 먼저& 오래된 게시글이 뒤로 오도록 게시글 정렬 순서 변경

--- src/main/java/com/example/spot/repository/PostRepository.java
+++ src/main/java/com/example/spot/repository/PostRepository.java
@@ -8,8 +8,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long>, PostRepositoryCustom {
+    // 정렬
+     Page<Post> findByBoardAndPostReportListIsEmptyOrderByCreatedAtDesc(Board board, Pageable pageable);
     Page<Post> findByBoardAndPostReportListIsEmpty(Board board, Pageable pageable);
 
+    // 정렬 조건 필요
+    Page<Post> findByPostReportListIsEmptyOrderByCreatedAtDesc(Pageable pageable);
     Page<Post> findByPostReportListIsEmpty(Pageable pageable); // 모든 게시글 조회
 
 }
